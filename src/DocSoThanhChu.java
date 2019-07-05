34fe4c41f1eb6b58a87a18f006f631ed315dcdc5
unknown <nguyenduc97hc>
2019-07-05 16:14:14 +0700

    bai tap

---
+++ src/DocSoThanhChu.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class DocSoThanhChu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int number = scanner.nextInt();
+
+        switch (number){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Tow");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");break;
+            case 9:
+                System.out.println("Nine");break;
+            case 10:
+                System.out.println("Ten");break;
+            case 11:
+                System.out.println("Eleven");break;
+            case 12:
+                System.out.println("Twele");break;
+            case 13:
+                System.out.println("Thirteen");break;
+            case 14:
+                System.out.println("FourTeen");break;
+            case 15:
+                System.out.println("FiveTeen");break;
+            case 16:
+                System.out.println("Sixteen");break;
+            case 17:
+                System.out.println("Seventeen");break;
+            case 18:
+                System.out.println("Eighteen");break;
+            case 19:
+                System.out.println("Nineteen");break;
+            case 20:
+                System.out.println("Twenty");break;
+        }
+    }
+}
